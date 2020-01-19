85d07e073fe58917bfd3acddc79132c775fd6486
Hadson Paredes <hadson1@gmail.com>
2020-01-19 17:58:06 -0500

    Add files via upload

---
+++ DEMO01_CRUD/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DEMO01_CRUD
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+            app.UseDeveloperExceptionPage();
+            app.UseMvc(router =>
+            {
+                router.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
